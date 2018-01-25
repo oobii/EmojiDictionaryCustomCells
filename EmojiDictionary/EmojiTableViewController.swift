064435e36ec2ae12da7ca334bbda9c2f57cb09d7
martynov <vvayfarer@yandex.com>
2018-01-24 21:15:39 -0500

    added more emoji

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -12,7 +12,24 @@ class EmojiTableViewController: UITableViewController {
     
     var emojis: [Emoji] = [
         Emoji(symbol: "😀", name: "Grinning face", description: "A typical smiley face", usage: "happiness"),
-        Emoji(symbol: "🐘", name: "Elephant", description: "A grey elephant", usage: "Good memory")]
+        Emoji(symbol: "🐘", name: "Elephant", description: "A grey elephant", usage: "Good memory"),
+        Emoji(symbol: "🐸", name: "Frog" , description: "Green frog" , usage: "Pond" ),
+        Emoji(symbol: "🐢", name: "Turtle" , description: "Wise turtle", usage: "slow"),
+        Emoji(symbol: "🍕", name:"Pizza" , description: "Slice of pizza", usage:"Food"),
+        Emoji(symbol:"🏐" , name: "Volleyball", description:"White volleyball", usage:"Sports" ),
+        Emoji(symbol:"⏰", name:"Alarm clock" , description:"Red clock" , usage:"Waking up"),
+        Emoji(symbol:"🇨🇦" , name:"Canada flag", description:"Red and white flag", usage:"Country" ),
+        Emoji(symbol:"👀", name:"Eyes" , description:"Two eyes" , usage:"To see" ),
+        Emoji(symbol:"🚲" , name:"Bycicle" , description:"Blue bike", usage:"Getting around"),
+        Emoji(symbol:"🌈" , name:"Rainbow", description:"seven coulors" , usage:"Rain and sun" ),
+        Emoji(symbol:"🎹" , name:"Musical keyboard" , description:"Piano" , usage:"Playing music" ),
+        Emoji(symbol:"📎" , name:"Paperclip" , description:"Grey clip" , usage:"Attachment"),
+        Emoji(symbol:"💚" , name:"Green heart" , description:"Shape" , usage:"Love" ),
+        Emoji(symbol:"🎂" , name:"Birthday cake" , description:"Desert" , usage:"Yummy" ),
+        Emoji(symbol:"☃️" , name:"Snowman" , description:"Winter" , usage:"Build" ),
+        Emoji(symbol:"🌹" , name:"Rose" , description:"Flower" , usage:"Smells good"),
+        Emoji(symbol:"🍍" , name:"Pineapple" , description:"Large fruit" , usage:"Wear a crown" ),
+        ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +64,7 @@ class EmojiTableViewController: UITableViewController {
         }
     }
     
-    // returm cell based on Model
+    // returm cell based on Emoji.swift Model
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EmojiCell", for: indexPath)
@@ -55,7 +72,8 @@ class EmojiTableViewController: UITableViewController {
         let emoji = emojis[indexPath.row]
         // Configure the cell...
         cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
-        cell.detailTextLabel?.text = emoji.description
+        cell.detailTextLabel?.text = "\(emoji.description) - \(emoji.usage)"
+        
         
         return cell
     }
